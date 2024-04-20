af9ed861945ca0fcb680e74dc2ab861612f02471
Qewertyy <kh786100@gmail.com>
2024-04-20 14:43:03 +0530

    chore: validation.

--- server/api/download/index.post.ts
+++ server/api/download/index.post.ts
@@ -4,7 +4,9 @@ import { download } from "~/server/downloader";
 export default eventHandler(async (event) => {
   const { platform, url } = await useValidatedQuery(event, {
     platform: z.string(),
-    url: z.string(),
+    url: z.string().url(
+      "not a valid url"
+    ),
   });
   const response = await download(platform, url);
   return event.respondWith(response);
